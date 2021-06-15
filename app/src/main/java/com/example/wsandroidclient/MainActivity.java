5de70633213565398bf8c32c1fbdbc1139620c8f
daoc <diego.ordonez@gmail.com>
2021-06-15 17:40:10 -0500

    Update

--- app/src/main/java/com/example/wsandroidclient/MainActivity.java
+++ app/src/main/java/com/example/wsandroidclient/MainActivity.java
@@ -11,24 +11,38 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
+/**
+ * Librería WebSockets:
+ * https://github.com/TooTallNate/Java-WebSocket
+ */
 public class MainActivity extends Activity {
 	WebSocketClient wsc;
 	TextView tv;
 	EditText et;
-	
-	
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.conversacion);
         et = (EditText) findViewById(R.id.msg);
-        
+    }
+
+    public void conectarWs(View v) {
+    	if(wsc != null && wsc.isOpen()) {
+    		wsc.close();
+    		wsc = null;
+		}
+    	wsc = newWebSocketClient();
+		wsc.connect();
+    }
+
+	public WebSocketClient newWebSocketClient() {
 		try {
 			wsc = new WebSocketClient(new URI("ws://10.0.2.2:8080/saludos")) {
 				@Override
 				public void onMessage(final String message) {
+					System.out.println("=> " + message);
 					runOnUiThread(new Runnable() {
 						@Override
 						public void run() {
@@ -40,11 +54,23 @@ public class MainActivity extends Activity {
 				@Override
 				public void onOpen(ServerHandshake handshake) {
 					System.out.println("Conexión abierta");
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							tv.append("*** Conexión abierta ***\n");
+						}
+					});
 				}
 
 				@Override
 				public void onClose(int code, String reason, boolean remote) {
 					System.out.println("Conexión cerrada: " + reason + code);
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							tv.append("*** Conexión cerrada: " + reason + " - " + code + " ***\n");
+						}
+					});
 				}
 
 				@Override
@@ -53,26 +79,24 @@ public class MainActivity extends Activity {
 				}
 
 			};
+			return wsc;
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		        
-    }
+		}
+		return null;
+	}
 
-    public void conectarWs(View v) {
-		wsc.connect();
-    }
-    
     public void enviarWs(View v) {
     	String msg = et.getText().toString();
 		wsc.send(msg);
     	tv.append("<= " + msg + "\n");
     }    
-    
+
     @Override
     protected void onDestroy() {
-    	if(wsc != null) {
+    	if(wsc != null && wsc.isOpen()) {
 			wsc.close();
+			wsc = null;
     	}
     	super.onDestroy();
     }
